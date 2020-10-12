3c57a330baf84dae5c12c8724369e6ea21e4e342
Басун Володимир <vbasoon@gmail.com>
2020-10-12 09:38:23 +0300

    correct index.js pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 import { Header } from "./components";
-import Home from "./pages/Home";
+import {Home} from "./pages";
 
 const items = ["М'ясні", "Вегетаріанські", "Гриль", "Гострі", "Закриті"];
 

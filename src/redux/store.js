cd542572e96250a0ee5b4f4b9882d311113c672c
Басун Володимир <vbasoon@gmail.com>
2020-10-27 15:22:23 +0200

    create store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,17 @@
 import React from 'react'
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+   switch (action.type) {
+      case 'INCREMENT':
+         return state + 1
+      case 'DECREMENT':
+         return state - 1
+      default:
+         return state
+   }
+}
+
+const store = createStore(counter);
+
+export default store
